01d76b4fb3f085c5753775900a728684ebf8cfbd
Alex Castaneda <acastaneda@NUMEDICS.OFFICE>
2016-05-03 13:52:22 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const TARGET = process.env.npm_lifecycle_event;
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  resolve: {
+    extensions: ['','.js','.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        // Test expects a RegExp! Note the slashes!
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }
+    ]
+  }
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    watchOptions: {
+      poll: true
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
+
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true // --save
+      })
+    ]
+  });
+}
+
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
