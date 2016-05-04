a8f855cb5a5124e3241c9c210e6a3520265564f5
Alexandar Castaneda <alex@alexthedar.com>
2016-05-04 13:48:59 -0700

    day 2 1st commit

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,29 +1,71 @@
 import React from 'react';
 import uuid from 'node-uuid';
+import Notes from './Notes';
 
 export default class App extends React.Component{
+    constructor(props){
+      super(props);
+
+      this.state = {
+        notes: [
+            {
+              id: uuid.v4(),
+              task: "Learn Webpack"
+            },
+            {
+              id: uuid.v4(),
+              task: "Learn React"
+            },
+            {
+              id: uuid.v4(),
+              task: "Laundry"
+            }
+          ]
+        };
+      }
     render(){
-    const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn Webpack'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Laundry'
-      },
-    ];
+    const notes = this.state.notes;
 
     return (
       <div>
-        <ul>{notes.map(note =>
-          <li key={note.id}>{note.task}</li>)}
-        </ul>
+      <button className="add-note" onClick={this.addNote}>+</button>
+        <Notes
+          notes={notes}
+            onEdit={this.editNote}
+            onDelete={this.deleteNote}
+        />
       </div>
-    )
+    );
   }
+
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: "New task"
+      }])
+    });
+  };
+
+deleteNote = (id,e) => {
+  e.stopPropagation();
+
+  this.setState({
+    notes: this.state.notes.filter(note => note.id !== id)
+  });
+};
+
+  editNote = (id,task) => {
+    if(!task.trim()) {
+      return;
+    }
+
+    const notes = this.state.notes.map(note => {
+      if(note.id === id && task){
+        note.task = task;
+      }
+      return note;
+    });
+    this.setState({notes});
+  };
 }
