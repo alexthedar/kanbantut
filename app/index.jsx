1cda7ac70aa5295a31db31b7628c5d2939195ac1
Alexandar Castaneda <alex@alexthedar.com>
2016-05-05 08:24:36 -0700

    3rd day 1st commit

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,11 @@ require('./main.css');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
+
+persist(alt, storage, 'app');
 
 ReactDOM.render(
   <App />,
