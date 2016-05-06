6205a458fef719edabe669db425227b7da48f7c5
Alexandar Castaneda <alex@alexthedar.com>
2016-05-06 08:27:32 -0700

    4th day 1st commit

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,61 +1,25 @@
 import React from 'react';
+import {DragSource} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
+
+const noteSource = {
+  beginDrag(props){
+    console.log('begin dragging note', props);
+    return{};
+  }
+};
+
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
+
 
 export default class Note extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      editing: false
-    };
-  }
   render() {
-    if(this.state.editing){
-      return this.renderEdit();
-    }
-    return this.renderNote();
-  }
-  renderEdit = () => {
-    return <input type="text"
-    ref={
-      element => element ? element.selectionStart = this.props.task.length : null
-    }
-    autoFocus={true}
-    defaultValue={this.props.task}
-    onBlur={this.finishEdit}
-    onKeyPress={this.checkEnter} />
-  };
-renderNote = () => {
-  const onDelete = this.props.onDelete;
+    const {connectDragSource, id, onMove, ...props} = this.props;
 
-  return (
-    <div onClick={this.edit}>
-      <span className="task">{this.props.task}</span>
-      {onDelete ? this.renderDelete() : null }
-    </div>
-  );
-};
-
-renderDelete=()=>{
-  return <button className="delete-note" onClick={this.props.onDelete}>x</button>;
-};
-
-edit = () => {
-  this.setState({
-    editing: true
-  });
-};
-checkEnter = (e) => {
-  if(e.key === 'Enter'){
-    this.finishEdit(e);
+    return connectDragSource(
+      <li {...props}>{props.children}</li>
+    );
   }
-};
-finishEdit = (e) => {
-  const value = e.target.value;
-  if(this.props.onEdit){
-    this.props.onEdit(value);
-    this.setState({
-      editing: false
-    });
-  }
-}
 }
